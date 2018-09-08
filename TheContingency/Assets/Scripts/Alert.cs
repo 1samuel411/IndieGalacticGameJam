fdf21754e88ddd29afb6d8d563a856664b923c25
Matt <carlt336@gmail.com>
2018-09-08 15:35:40 -0400

    alert

---
+++ TheContingency/Assets/Scripts/Alert.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Alert : MonoBehaviour 
+{
+	
+	public Text alertText;
+	
+    void Start ()
+    {
+        string[] alertArray1 = {"air pressure","attitude","velocity"};
+		string[] alertArray2 = {"low","high"};
+		int rand1 = UnityEngine.Random.Range(0,2);
+		int rand2 = UnityEngine.Random.Range(0,1);
+		
+		string alertDisplay = "Your " + alertArray1[rand1] + " is "+alertArray2[rand2]+". Make a correction!";
+		Debug.Log(alertDisplay);
+		alertText.text = alertDisplay;
+    }
+}
