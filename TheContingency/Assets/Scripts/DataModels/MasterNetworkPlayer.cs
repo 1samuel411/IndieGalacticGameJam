6a108c87d6fb2dd9a9febd171cec2f13008d64b8
Samuel Arminana <armi.sam99@gmail.com>
2018-09-08 17:20:38 -0400

    + DataModels to assets

---
+++ TheContingency/Assets/Scripts/DataModels/MasterNetworkPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SNetwork.Server;
+
+namespace SNetwork
+{
+    [Serializable]
+    public class MasterNetworkPlayer
+    {
+        public List<KeyValuePairs> data = new List<KeyValuePairs>();
+
+        public int id = -1;
+        public string username;
+
+        public MasterNetworkPlayer(int id, string username)
+        {
+            this.id = id;
+            this.username = username;
+        }
+
+        public MasterNetworkPlayer(int id)
+        {
+            this.id = id;
+            username = "";
+        }
+
+        public MasterNetworkPlayer()
+        {
+            id = -1;
+            username = "";
+        }
+    }
+}
