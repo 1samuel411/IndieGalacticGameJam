2b7afbcf5a85fed749862bc7936d3c06b4515a3e
Matt <carlt336@gmail.com>
2018-09-08 16:06:00 -0400

    clean alert

--- TheContingency/Assets/Scripts/Alert.cs
+++ TheContingency/Assets/Scripts/Alert.cs
@@ -4,18 +4,13 @@ using System.Collections;
 
 public class Alert : MonoBehaviour 
 {
-	
 	public Text alertText;
-	
     void Start ()
     {
-        string[] alertArray1 = {"air pressure","attitude","velocity"};
+		string[] alertArray1 = {"air pressure","attitude","velocity"};
 		string[] alertArray2 = {"low","high"};
 		int rand1 = UnityEngine.Random.Range(0,alertArray1.Length);
 		int rand2 = UnityEngine.Random.Range(0,alertArray2.Length);
-		
-		string alertDisplay = "Your " + alertArray1[rand1] + " is "+alertArray2[rand2]+". Make a correction!";
-		Debug.Log(alertDisplay);
-		alertText.text = alertDisplay;
+		alertText.text = "Your " + alertArray1[rand1] + " is "+alertArray2[rand2]+". Make a correction!";
     }
 }
