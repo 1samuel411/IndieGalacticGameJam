43aebe443e97089b0452e1fc66d8d3e619923d4e
David Flax <flax41@gmail.com>
2018-09-08 20:55:10 -0400

    ~Renamed Alert Script

---
+++ TheContingency/Assets/Scripts/AlertText.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AlertText : MonoBehaviour 
+{
+	public Text alertText;
+
+    void Start ()
+    {
+
+    }
+
+    void Update()
+    {
+    }
+}
